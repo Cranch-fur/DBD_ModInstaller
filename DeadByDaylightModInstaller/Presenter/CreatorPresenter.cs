43b90eefcad1b070efb8b26c35caf6f58993074f
Mutek34 <mutek34@gmail.com>
2021-09-24 08:55:29 +0100

    Initial version of multiple texture package creator

--- DeadByDaylightModInstaller/Presenter/CreatorPresenter.cs
+++ DeadByDaylightModInstaller/Presenter/CreatorPresenter.cs
@@ -16,6 +16,7 @@ namespace Dead_By_Daylight_Mod_Installer.Presenter
         private readonly ICreatorView view;
         private readonly IMessageBoxService messageBoxService;
         private readonly IPickerService pickerService;
+        private List<ModListItem> package;
 
         public CreatorPresenter(ICreatorView view, IMessageBoxService messageBoxService, IPickerService pickerService)
         {
@@ -23,71 +24,101 @@ namespace Dead_By_Daylight_Mod_Installer.Presenter
             this.view.Presenter = this;
             this.messageBoxService = messageBoxService;
             this.pickerService = pickerService;
+
+            package = new List<ModListItem>();
+            AddMod();
         }
 
-        public void PickPakFile()
+        public void AddMod()
+        {
+            package.Add(new ModListItem
+            {
+                Rows = new List<ModListItem.Row>
+                {
+                    new ModListItem.Row
+                    {
+                        DisplayName = nameof(ModPackage.Mod.Title),
+                        Name = ModListItem.Row.TitleRowName,
+                        Data = "Package",
+                    },
+                    new ModListItem.Row
+                    {
+                        DisplayName = nameof(ModPackage.Mod.PakName),
+                        Name = ModListItem.Row.PakFileNameRowName,
+                        Data = "",
+                    },
+                    new ModListItem.Row
+                    {
+                        DisplayName = "Original ubulk path",
+                        Name = ModListItem.Row.OriginalUbulkPathRowName,
+                        Data = "",
+                    },
+                    new ModListItem.Row
+                    {
+                        DisplayName = "Modified ubulk path",
+                        Name = ModListItem.Row.ModifiedUbulkPathRowName,
+                        Data = "",
+                    },
+                }
+            });
+            view.ModListItems = package;
+        }
+
+        public void PickPakFile(ref ModListItem.Row row)
         {
             if(pickerService.PickFilePath(out string pakFilePath, "Pak file|*.pak") == Enums.PickResult.Ok)
             {
-                view.PakFileName = Path.GetFileName(pakFilePath);
+                row.Data = Path.GetFileName(pakFilePath);
             }
         }
 
-        public void PickOriginalFile()
+        public void PickUbulkFile(ref ModListItem.Row row)
         {
             if (pickerService.PickFilePath(out string originalFilePath, "ubulk (*.ubulk)|*.ubulk|All files (*.*)|*.*") == Enums.PickResult.Ok)
             {
-                view.OriginalFilePath = originalFilePath;
-            }
-        }
-
-        public void PickModifiedFile()
-        {
-            if (pickerService.PickFilePath(out string modifiedFilePath, "ubulk (*.ubulk)|*.ubulk|All files (*.*)|*.*") == Enums.PickResult.Ok)
-            {
-                view.ModifiedFilePath = modifiedFilePath;
+                row.Data = originalFilePath;
             }
         }
 
         public void CreateModPackage()
         {
-            if(string.IsNullOrWhiteSpace(view.ModTitle) || string.IsNullOrWhiteSpace(view.PakFileName)
-                || string.IsNullOrWhiteSpace(view.ModTitle) || string.IsNullOrWhiteSpace(view.PakFileName))
-            {
-                messageBoxService.ShowMessage("One of the fields is empty");
-                return;
-            }
+            //if(string.IsNullOrWhiteSpace(view.ModTitle) || string.IsNullOrWhiteSpace(view.PakFileName)
+            //    || string.IsNullOrWhiteSpace(view.ModTitle) || string.IsNullOrWhiteSpace(view.PakFileName))
+            //{
+            //    messageBoxService.ShowMessage("One of the fields is empty");
+            //    return;
+            //}
 
-            var pickResult = pickerService.PickSaveFilePath(out string filePath);
-            if (pickResult == Enums.PickResult.Ok)
-            {
-                var modPackage = new ModPackage
-                {
-                    Mods = new List<ModPackage.Mod>
-                    {
-                        new ModPackage.Mod
-                        {
-                            Title = view.ModTitle,
-                            PakName = view.PakFileName,
-                            OriginalBytes = File.ReadAllBytes(view.OriginalFilePath),
-                            ModifiedBytes = File.ReadAllBytes(view.ModifiedFilePath),
-                        }
-                    }
-                };
+            //var pickResult = pickerService.PickSaveFilePath(out string filePath);
+            //if (pickResult == Enums.PickResult.Ok)
+            //{
+            //    var modPackage = new ModPackage
+            //    {
+            //        Mods = new List<ModPackage.Mod>
+            //        {
+            //            new ModPackage.Mod
+            //            {
+            //                Title = view.ModTitle,
+            //                PakName = view.PakFileName,
+            //                OriginalBytes = File.ReadAllBytes(view.OriginalFilePath),
+            //                ModifiedBytes = File.ReadAllBytes(view.ModifiedFilePath),
+            //            }
+            //        }
+            //    };
 
-                try
-                {
-                    File.WriteAllText(filePath, JsonConvert.SerializeObject(modPackage));
-                }
-                catch(Exception ex)
-                {
-                    messageBoxService.ShowMessage(ex.Message);
-                }
-            }
-            else if(pickResult == Enums.PickResult.None)
-            {
-                messageBoxService.ShowMessage("Failed to get save file path");
-            }
+            //    try
+            //    {
+            //        File.WriteAllText(filePath, JsonConvert.SerializeObject(modPackage));
+            //    }
+            //    catch(Exception ex)
+            //    {
+            //        messageBoxService.ShowMessage(ex.Message);
+            //    }
+            //}
+            //else if(pickResult == Enums.PickResult.None)
+            //{
+            //    messageBoxService.ShowMessage("Failed to get save file path");
+            //}
         }
     }
 }
--- DeadByDaylightModInstaller/View/CreatorForm.cs
+++ DeadByDaylightModInstaller/View/CreatorForm.cs
@@ -1,8 +1,11 @@
-﻿using Dead_By_Daylight_Mod_Installer.Presenter;
+﻿using Dead_By_Daylight_Mod_Installer.Model;
+using Dead_By_Daylight_Mod_Installer.Presenter;
 using Dead_By_Daylight_Mod_Installer.View;
 using Microsoft.Win32;
 using Newtonsoft.Json.Linq;
 using System;
+using System.Collections;
+using System.Collections.Generic;
 using System.Data;
 using System.IO;
 using System.Linq;
@@ -13,36 +16,61 @@ namespace Dead_By_Daylight_Mod_Installer
 {
     public partial class CreatorForm : Form, ICreatorView
     {
+        public List<ModListItem> ModListItems
+        {
+            get => this.modsTreeListView.Roots as List<ModListItem>;
+            set => this.modsTreeListView.Roots = value;
+        }
+
+
         public CreatorForm()
         {
             InitializeComponent();
+
+            this.modsTreeListView.ButtonClick += ModsTreeListView_ButtonClick;
+
+            this.actionColumn.AspectGetter = model => model is ModListItem.Row ? "Change" : null;
+
+            this.displayColumn.AspectGetter = model =>
+            {
+                if(model is ModListItem modModel)
+                {
+                    return modModel.Rows.FirstOrDefault()?.Data ?? string.Empty;
+                }
+                else if (model is ModListItem.Row rowModel)
+                {
+                    return rowModel.DisplayName;
+                }
+                return string.Empty;
+            };
+            this.dataColumn.AspectGetter = model =>
+            {
+                if (model is ModListItem.Row rowModel)
+                {
+                    return rowModel.Data;
+                }
+                return string.Empty;
+            };
+            this.dataColumn.AspectPutter = (model, newValue) =>
+            {
+                if (model is ModListItem.Row rowModel && rowModel.Name == ModListItem.Row.TitleRowName)
+                {
+                    rowModel.Data = newValue;
+                }
+            };
+            this.modsTreeListView.CanExpandGetter =  model => model is ModListItem;
+            this.modsTreeListView.ChildrenGetter = model =>
+            {
+                if(model is ModListItem modModel)
+                {
+                    return modModel.Rows;
+                }
+                return null;
+            };
+            this.modsTreeListView.ShowItemToolTips = true;
         }
 
         public CreatorPresenter Presenter { private get; set; }
-
-        public string ModTitle
-        {
-            get => this.TitleTextBox.Text;
-            set => this.TitleTextBox.Text = value;
-        }
-        
-        public string PakFileName
-        {
-            get => this.pakNameTextBox.Text;
-            set => this.pakNameTextBox.Text = value;
-        }
-
-        public string OriginalFilePath
-        {
-            get => this.OriginalTexturePathTextBox.Text;
-            set => this.OriginalTexturePathTextBox.Text = value;
-        }
-
-        public string ModifiedFilePath
-        {
-            get => this.ModifiedTexturePathTextBox.Text;
-            set => this.ModifiedTexturePathTextBox.Text = value;
-        }
 
         private void CloseToolbarButton_Click(object sender, EventArgs e)
         {
@@ -64,24 +92,43 @@ namespace Dead_By_Daylight_Mod_Installer
             WndProc(ref mouse);
         }
 
-        private void PakNameTextBox_Click(object sender, EventArgs e)
-        {
-            Presenter.PickPakFile();
-        }
-
-        private void OriginalTexturePathTextBox_Click(object sender, EventArgs e)
-        {
-            Presenter.PickOriginalFile();
-        }
-
-        private void ModifiedTexturePathTextBox_Click(object sender, EventArgs e)
-        {
-            Presenter.PickModifiedFile();
-        }
-
-        private void CreateJsonButton_Click(object sender, EventArgs e)
+        private void GeneratePackageButton_Click(object sender, EventArgs e)
         {
             Presenter.CreateModPackage();
+        }
+
+        private void AddModButton_Click(object sender, EventArgs e)
+        {
+            Presenter.AddMod();
+        }
+
+        private void ModsTreeListView_ButtonClick(object sender, BrightIdeasSoftware.CellClickEventArgs e)
+        {
+            var row = e.Model as ModListItem.Row;
+            if (row.Name == ModListItem.Row.TitleRowName)
+            {
+                this.modsTreeListView.StartCellEdit(e.Item, 1);
+            }
+            else if (row.Name == ModListItem.Row.PakFileNameRowName)
+            {
+                Presenter.PickPakFile(ref row);
+            }
+            else if (row.Name == ModListItem.Row.OriginalUbulkPathRowName || row.Name == ModListItem.Row.ModifiedUbulkPathRowName)
+            {
+                Presenter.PickUbulkFile(ref row);
+            }
+        }
+
+        private void modsTreeListView_CellEditStarting(object sender, BrightIdeasSoftware.CellEditEventArgs args)
+        {
+            // Left align the edit control
+            args.Control.Location = args.CellBounds.Location;
+
+            // Readjust the size of the control to fill the whole cell if CellEditUseWholeCellEffective is enabled
+            if (args.Column.CellEditUseWholeCellEffective)
+            {
+                args.Control.Size = args.CellBounds.Size;
+            }
         }
     }
 }
