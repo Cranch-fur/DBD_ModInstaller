da320357bb5b7c6e782c215ad42c1b495d6be5ef
Mutek34 <mutek34@gmail.com>
2021-09-24 08:55:29 +0100

    Initial version of multiple texture package creator

---
+++ DeadByDaylightModInstaller/Model/ModListItem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dead_By_Daylight_Mod_Installer.Model
+{
+    public class ModListItem
+    {
+        public class Row
+        {
+            public const string TitleRowName = "Title";
+            public const string PakFileNameRowName = "PakFileName";
+            public const string OriginalUbulkPathRowName = "OriginalUbulkPath";
+            public const string ModifiedUbulkPathRowName = "ModifiedUbulkPath";
+            public ModListItem Parent { get; set; }
+            public string DisplayName { get; set; }
+            public string Name { get; set; }
+            public object Data { get; set; }
+        }
+
+        public List<Row> Rows { get; set; }
+    }
+}
--- DeadByDaylightModInstaller/View/ICreatorView.cs
+++ DeadByDaylightModInstaller/View/ICreatorView.cs
@@ -1,4 +1,5 @@
-﻿using Dead_By_Daylight_Mod_Installer.Presenter;
+﻿using Dead_By_Daylight_Mod_Installer.Model;
+using Dead_By_Daylight_Mod_Installer.Presenter;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,7 @@ namespace Dead_By_Daylight_Mod_Installer.View
 {
     public interface ICreatorView
     {
-        string PakFileName { get; set; }
         CreatorPresenter Presenter { set; }
-        string OriginalFilePath { get; set; }
-        string ModifiedFilePath { get; set; }
-        string ModTitle { get; set; }
+        List<ModListItem> ModListItems { get; set; }
     }
 }
